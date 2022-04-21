96690ac7024692f8f7f590f838b393dbf1721273
Michael Kobelev <misha.kobelev.99@gmail.com>
2022-04-21 09:44:17 +0300

    Update mongo trigger

--- mongo/trigger_bot_update.js
+++ mongo/trigger_bot_update.js
@@ -3,12 +3,17 @@
 exports = function(changeEvent) {
   const auth_secret = context.values.get("auth_secret_value");
   const url = context.values.get('event_url') + 'doc/' + changeEvent.documentKey._id.toString();
+  const updatedFields = changeEvent.updateDescription?.updatedFields;
 
-  return context.http.post({
-    url: url, 
-    headers: {
-      "Content-Type": [ "application/json" ],
-      "Authorization": [`Bearer ${auth_secret}`],
-    }
-  });
+  if (updatedFields?.src || updatedFields?.token || updatedFields?.status) {
+    return context.http.post({
+      url: url, 
+      headers: {
+        "Content-Type": [ "application/json" ],
+        "Authorization": [`Bearer ${auth_secret}`],
+      }
+    });
+  }
+
+  return null;
 }

2218d02a5eeb1d4603ef08dee10d07f1a165bc87
Michael Kobelev <misha.kobelev.99@gmail.com>
2022-04-28 15:26:27 +0300

    Fix reporting metrics in go

--- bot-execution/services/prometheus.go
+++ bot-execution/services/prometheus.go
@@ -5,20 +5,6 @@ import "github.com/prometheus/client_golang/prometheus"
 var MetricTotalRequests = prometheus.NewCounter(
 	prometheus.CounterOpts{
 		Name: "http_requests_total",
-		Help: "Number of get requests.",
-	},
-)
-
-var MetricTaskQueueSize = prometheus.NewGauge(
-	prometheus.GaugeOpts{
-		Name: "task_queue_size",
-		Help: "Number of tasks in redis queue",
-	},
-)
-
-var MetricNumberKeys = prometheus.NewGauge(
-	prometheus.GaugeOpts{
-		Name: "redis_key_number",
-		Help: "Number of keys in redis.",
+		Help: "Number of get requests to update endpoint.",
 	},
 )
--- bot-execution/update_http/main.go
+++ bot-execution/update_http/main.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	"bot-execution/scheduling"
 	"bot-execution/services"
 	"context"
 	"fmt"
 	"github.com/go-chi/chi/middleware"
-	"github.com/go-co-op/gocron"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"log"
@@ -22,30 +20,14 @@ import (
 func prometheusMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		next.ServeHTTP(w, r)
-		services.MetricTotalRequests.Inc()
+		if r.URL.Path != "/metrics" {
+			services.MetricTotalRequests.Inc()
+		}
 	})
-}
-
-func collectRedisMetrics() {
-	redis := services.NewRedis()
-	dbSize, err := redis.DBSize()
-	if err != nil {
-		return
-	}
-	services.MetricNumberKeys.Set(float64(dbSize))
-
-	queue := scheduling.NewRedisTaskQueue()
-	size, err := queue.Size()
-	if err != nil {
-		return
-	}
-	services.MetricTaskQueueSize.Set(float64(size))
 }
 
 func init() {
 	prometheus.MustRegister(services.MetricTotalRequests)
-	prometheus.MustRegister(services.MetricNumberKeys)
-	prometheus.MustRegister(services.MetricTaskQueueSize)
 }
 
 func NewRouter() http.Handler {
@@ -72,10 +54,6 @@ func main() {
 		log.Fatalf("sentry.Init: %s", err)
 	}
 	defer sentry.Flush(2 * time.Second)
-
-	s := gocron.NewScheduler(time.UTC)
-	s.Every("1m").Do(collectRedisMetrics)
-	s.StartAsync()
 
 	port := services.GetEnvFallback("PORT", "8088")
 	srv := &http.Server{
